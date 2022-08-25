0487575f953fc9a1c89ccb1725c4c0be7d791668
MayurAnklekar <mayuranklekar1010@gmail.com>
2022-08-25 17:43:49 +0530

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axiosConfig from "./services/axiosConfig";
 
 function App() {
+  const [data, setData] = useState('')
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const Data = await axiosConfig.get('/users');
+      console.log(Data)
+      setData(Data);
+    }
+    fetchData();
+  },[data])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data?(<>{data}</>):(<>Loading...</>)}
     </div>
   );
 }

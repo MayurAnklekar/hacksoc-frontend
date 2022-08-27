7c2cddf1c3abed5b0f2257bca40ceb23c8cdbc78
Meeth Davda <mjdavda02@gmail.com>
2022-08-27 18:10:55 +0530

    books

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Home from "../pages/Home";
+import Books from "../pages/Books";
+
 // import Appbar from "../components/Appbar/Appbar";
 // import Profile from "../pages/Profile/Profile";
 // import AllUsers from "../components/AllUsers/AllUsers";
@@ -8,25 +10,25 @@ import Home from "../pages/Home";
 // import SinglePost from "../pages/SinglePost/SinglePost";
 
 const Router = () => {
-	// const {
-	// 	modal: { isSidebarVisible }
-	// } = useSelector(state => state);
+  // const {
+  // 	modal: { isSidebarVisible }
+  // } = useSelector(state => state);
 
-
-	return (
-		<>
-			{/* <div className={isSidebarVisible ? "sidebar visible" : "sidebar"}>
+  return (
+    <>
+      {/* <div className={isSidebarVisible ? "sidebar visible" : "sidebar"}>
 				<AllUsers />
 			</div>
 			<Appbar /> */}
-			<Routes>
-				<Route path="/" element={<Home />} />
-				{/* <Route path="/user/:id" element={<Profile />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Books />} />
+        {/* <Route path="/user/:id" element={<Profile />} />
 				<Route path="/chat" element={<Chat />} />
         		<Route path="/post/:id" element={<SinglePost />} /> */}
-			</Routes>
-		</>
-	);
+      </Routes>
+    </>
+  );
 };
 
 export default Router;

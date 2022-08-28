10c06bafca2877db582e717e1e56dcc5f51d670f
Meeth Davda <mjdavda02@gmail.com>
2022-08-28 08:58:09 +0530

    report card

--- src/components/adminDashboard/ReportCard.js
+++ src/components/adminDashboard/ReportCard.js
@@ -1,20 +1,26 @@
 import React from "react";
+import axiosConfig from "../../services/axiosConfig";
 
-function ReportCard() {
+function ReportCard(props) {
+  const handler = async () => {
+    const res = await axiosConfig.post("/approval", {
+      bookID: props.bk,
+      uid: props.uid,
+      eval: "approved",
+    });
+    console.log(res);
+  };
+
   return (
     <div>
       <div className="flex flex-col h-[30em] w-[25em] justify-center  items-center bg-gray-100 m-5 rounded-tl-3xl">
-        <h2 className="m-5 my-5 text-3xl">Title</h2>
-        <p className="m-5">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos corporis
-          ipsa fugit natus sunt. Sint aliquid totam et incidunt nulla debitis
-          officiis maiores? Qui placeat maiores culpa facere reprehenderit
-          exercitationem, iusto provident delectus reiciendis ex laborum
-          explicabo necessitatibus quo nemo deleniti fugiat atque repudiandae.
-          Obcaecati explicabo magnam repudiandae perferendis adipisci!
-        </p>
+        <h2 className="m-5 my-5 text-3xl">{props.title}</h2>
+        <p className="m-5">{props.summary}</p>
         <div className="flex flex-row">
-          <button className="bg-yellow-300 w-24 h-10 rounded-md mx-4">
+          <button
+            className="bg-yellow-300 w-24 h-10 rounded-md mx-4"
+            onClick={handler}
+          >
             Approve
           </button>
           <button className="bg-yellow-300 w-24 h-10 rounded-md ">

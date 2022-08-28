51bf593c4d0ebf2acf4141630a01322d9e4e0136
Pradhan A N <an.pradhan2002@gmail.com>
2022-08-28 06:17:19 +0530

    sidebar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,11 +10,11 @@ export default function Navbar() {
         <div className="navbar-position">
         <div className="navbar-container">
             <div>
-                <Typography variant="h4" sx={{color:"black", fontWeight:"700"}}>Humanity</Typography>
+                <Typography variant="h4" sx={{color:"rgb(105, 105, 76)", fontWeight:"700"}}>Humanity</Typography>
             </div>
             <div className="navbar-options">
-            <button ><Typography variant="h7" sx={{color:"black", fontWeight:"700"}}>History</Typography></button>
-                <button ><Typography variant="h7" sx={{color:"black", fontWeight:"700"}} onClick={signOutFromApp}>LogOut</Typography></button>
+            <button ><Typography variant="h7" sx={{color:"rgb(105, 105, 76)", fontWeight:"700"}}>History</Typography></button>
+                <button ><Typography variant="h7" sx={{color:"rgb(105, 105, 76)", fontWeight:"700"}} onClick={signOutFromApp}>LogOut</Typography></button>
                 
             </div>
          
